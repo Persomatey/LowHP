6c652faa6ea3108482950f035dbef938e3b6fc66
Hunter Goodin <huntergoodin1@hotmail.com>
2023-01-19 17:46:52 -0800

    Making header sub buttons float to the left. Removing individual episode changes, making it work with dynamically loaded html instead. Making header load the vectorized templogo. Removed extra functions from scripts. Added the functionality os some functions to existing functions instead.

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ function LoadFooter()
 	.then(response => response.text())
 }
 
-function ParseRSSThenReturnIndex(url)
+function ParseRSSThenReturnIndex(url, passedNum)
 {
 	fetch(url)
 		.then(response => response.text())
@@ -30,10 +30,7 @@ function ParseRSSThenReturnIndex(url)
 			let items = channel.querySelectorAll('item');
 
 			html += '<p>';
-
-			var num = window.location.pathname;
-			num = GetEpisodeNumberFromURL(num); 
-			num = num.replace(".html", ""); 
+			num = passedNum;
 			console.log(num);
 			num = items.length - num;
 			console.log(num); 
@@ -46,15 +43,13 @@ function ParseRSSThenReturnIndex(url)
 			let description = item.querySelector('description').textContent;
 			let content = item.querySelector('content').textContent;
 			let ytLink = item.querySelector('ytlink').textContent;
-			let vidID = GetVideoID(ytLink); 
+			let vidID = url.split("v=")[1]; 
 
 			html += `<h2 style="padding-bottom:0px;">${title}</h2>`;
 			html += `<h3 style="text-align:left;">${pubDate}</h3>`; 
 			html += `<h4 style="text-align:left;">${description}</h4>`; 
 			html += `<iframe width="560" height="315" src="https://www.youtube.com/embed/${vidID}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
 			html += `${content}`;
-			html += `<br><br><br>`
-
 			html += '</p>';
 
 			return html;
@@ -75,13 +70,14 @@ function ParseAllRSS(url)
 			let items = channel.querySelectorAll('item');
 			html += '<h1>' + "ARCHIVE:" + '</h1>';
 			html += '<p>';
+			// let channelNum = title.split(":")[0];
 
 			items.forEach(item => {
 				let title = item.querySelector('title').textContent;
-				let episodeNum = GetEpisodeNumber(title); 
+				let episodeNum = title.split(":")[0]; 
 				let url = `episodes/${episodeNum}.html` 
 
-				html += `<A href="${url}"> <h2 style="padding-bottom:0px;">${title}</h2></A>`;
+				html += `<A href="#" onclick="OpenNewPage('${title.split(":")[0]}')"> <h2 style="padding-bottom:0px;">${title}</h2></A>`;
 				html += `<br>`
 			});
 			html += '</p>';
@@ -117,14 +113,14 @@ function ParseRSSToCount(url, count)
 					let description = item.querySelector('description').textContent;
 					let content = item.querySelector('content').textContent;
 					let ytLink = item.querySelector('ytlink').textContent;
-					let vidID = GetVideoID(ytLink); 
+					let vidID = ytLink.split("v=")[1]; 
 
 					html += `<h2 style="padding-bottom:0px;">${title}</h2>`;
 					html += `<h3 style="text-align:left;">${pubDate}</h3>`; 
 					html += `<h4 style="text-align:left;">${description}</h4>`; 
 					html += `<iframe width=560 height=315 src="https://www.youtube.com/embed/${vidID}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
 					html += `${content}`;
-					html += `<br><br><br>`
+					html += `<br><br>`
 				}
 			});
 
@@ -137,20 +133,56 @@ function ParseRSSToCount(url, count)
 		.then(text => document.getElementById('rss').innerHTML = text);
 }
 
-function GetVideoID(url) 
+function OpenNewPage(passedNum) 
 {
-	var ret = url.split("v=")[1];
-	return ret;
-}
+	var newWindow = window.open();
+	newWindow.onload = function() 
+	{
+		var html = 
+		`
+		<!doctype html> 
+		<html style="background-color:#283850;"> 
+		
+			<head>
+				<title>Episode ${passedNum}</title>
+				<link rel="stylesheet" type="text/css" href="style.css">
+				<script src="scripts.js"></script>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			</head>
+		
+			<body style="background-color:#283850;" >
+		
+				<!-- Header -->
+		
+				<script>LoadHeader()</script>
+				<div id="header"></div>
+		
+				<!-- Contents -->
+		
+				<div class="row">
+					<div class="columnside">
+						<p></p>
+					</div>
+		
+					<div class="column">
+						<script>ParseRSSThenReturnIndex("testrss.rss", ${passedNum})</script>
+						<div id="rss" style="padding-left:15px"></div>
+						<a class="hunterlink" style="font-size: 35px; padding-right:10px; padding-left:15px;" href="archive.html"><b>ARCHIVE</b></a>
+						<br><br><br>
+					</div>
+		
+					<div class="columnside">
+						<p></p>
+					</div>
+				</div>
+		
+				<!-- Footer -->
+		
+			</body>
+		</html>
+		`;
 
-function GetEpisodeNumberFromURL(url) 
-{
-	var ret = url.split("episodes/")[1];
-	return ret;
-}
-
-function GetEpisodeNumber(str)
-{
-	var ret = str.split(":")[0];
-	return ret; 
+		newWindow.document.write(html);
+		window.close();
+	}
 }
